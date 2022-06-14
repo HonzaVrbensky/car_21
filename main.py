80f1bc84a495b03179269c5d1c4bdbfe338d6183
HonzaVrbensky <91247842+HonzaVrbensky@users.noreply.github.com>
2022-06-14 21:47:23 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,74 @@
+speedFactor = 80
+l = DigitalPin.P13
+r = DigitalPin.P14
+pin_Trig = DigitalPin.P8
+pin_Echo = DigitalPin.P15
+whiteline = 1
+connected = 0
+manual = 0
+strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+pins.set_pull(l, PinPullMode.PULL_NONE)
+pins.set_pull(r, PinPullMode.PULL_NONE)
+bluetooth.start_uart_service()
+basic.show_string("S")
+
+def motor_run(left = 0, right = 0, speed_factor = 80):
+    PCAmotor.motor_run(PCAmotor.Motors.M1, Math.map(Math.constrain(left * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+    PCAmotor.motor_run(PCAmotor.Motors.M2, Math.map(Math.constrain(-1 * right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+
+def motor_stop():
+    PCAmotor.motor_stop(PCAmotor.Motors.M1)
+    PCAmotor.motor_stop(PCAmotor.Motors.M2)
+
+def on_bluetooth_connected():
+    global connected
+    basic.show_icon(IconNames.HEART)
+    connected = 1
+    while connected == 1:
+        uartData = bluetooth.uart_read_until(serial.delimiters(Delimiters.HASH))
+        console.log_value("data", uartData)
+        if uartData == "A":
+            motor_run(-70, 80, -80)
+            basic.pause(500)
+            motor_stop()
+        elif uartData == "C":
+            motor_run(-70, 0, -80)
+            basic.pause(300)
+            motor_stop()
+        elif uartData == "D":
+            motor_run(0, 80, -80)
+            basic.pause(300)
+            motor_stop()
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
+
+def on_bluetooth_disconnected():
+    global connected
+    basic.show_icon(IconNames.SAD)
+    connected = 0
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
+def manual_movement():
+    global manual
+    if manual == 0:
+        maual = 1
+    else:
+        manual = 0
+input.on_button_pressed(Button.AB, manual_movement)
+
+def movement():
+    if connected ==0:
+        def on_forever():
+            global l, r
+            obstacle_distance = sonar.ping(pin_Trig, pin_Echo, PingUnit.CENTIMETERS, 100)
+            l = pins.digital_read_pin(DigitalPin.P13)
+            r = pins.digital_read_pin(DigitalPin.P14)
+            if l == 1 and r == 0:
+                motor_run(0, 80, -80)
+            elif l == 0 and r == 1:
+                motor_run(-70, 0, -80)
+            else:
+                motor_run(-70, 80, -80)
+            basic.pause(50) #reakční frekvence 20 Hz
+            print(l +" "+ r)
+        basic.forever(on_forever)
+basic.forever(movement)
---
+++ main.ts
@@ -0,0 +1,84 @@
+let speedFactor = 80
+let l = DigitalPin.P13
+let r = DigitalPin.P14
+let pin_Trig = DigitalPin.P8
+let pin_Echo = DigitalPin.P15
+let whiteline = 1
+let connected = 0
+let manual = 0
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+pins.setPull(l, PinPullMode.PullNone)
+pins.setPull(r, PinPullMode.PullNone)
+bluetooth.startUartService()
+basic.showString("S")
+function motor_run(left: number = 0, right: number = 0, speed_factor: number = 80) {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, Math.map(Math.constrain(left * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+    PCAmotor.MotorRun(PCAmotor.Motors.M2, Math.map(Math.constrain(-1 * right * (speedFactor / 100), -100, 100), -100, 100, -255, 255))
+}
+
+function motor_stop() {
+    PCAmotor.MotorStop(PCAmotor.Motors.M1)
+    PCAmotor.MotorStop(PCAmotor.Motors.M2)
+}
+
+bluetooth.onBluetoothConnected(function on_bluetooth_connected() {
+    let uartData: string;
+    
+    basic.showIcon(IconNames.Heart)
+    connected = 1
+    while (connected == 1) {
+        uartData = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
+        console.logValue("data", uartData)
+        if (uartData == "A") {
+            motor_run(-70, 80, -80)
+            basic.pause(500)
+            motor_stop()
+        } else if (uartData == "C") {
+            motor_run(-70, 0, -80)
+            basic.pause(300)
+            motor_stop()
+        } else if (uartData == "D") {
+            motor_run(0, 80, -80)
+            basic.pause(300)
+            motor_stop()
+        }
+        
+    }
+})
+bluetooth.onBluetoothDisconnected(function on_bluetooth_disconnected() {
+    
+    basic.showIcon(IconNames.Sad)
+    connected = 0
+})
+input.onButtonPressed(Button.AB, function manual_movement() {
+    let maual: number;
+    
+    if (manual == 0) {
+        maual = 1
+    } else {
+        manual = 0
+    }
+    
+})
+basic.forever(function movement() {
+    if (connected == 0) {
+        basic.forever(function on_forever() {
+            
+            let obstacle_distance = sonar.ping(pin_Trig, pin_Echo, PingUnit.Centimeters, 100)
+            l = pins.digitalReadPin(DigitalPin.P13)
+            r = pins.digitalReadPin(DigitalPin.P14)
+            if (l == 1 && r == 0) {
+                motor_run(0, 80, -80)
+            } else if (l == 0 && r == 1) {
+                motor_run(-70, 0, -80)
+            } else {
+                motor_run(-70, 80, -80)
+            }
+            
+            basic.pause(50)
+            // reakční frekvence 20 Hz
+            console.log(l + " " + r)
+        })
+    }
+    
+})
